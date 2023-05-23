134bfe2980413f3dea2d1e42b7cc8f878ca2ab94
0juniii <dldudwns0207@gmail.com>
2023-05-23 17:05:05 +0900

    tayotayo github & naver map

---
+++ app/src/main/java/com/cookandroid/project_mobile/MainActivity.java
@@ -0,0 +1,60 @@
+package com.cookandroid.project_mobile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /** toolbar **/
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); //액티비티의 앱바(App Bar)로 지정
+        ActionBar actionBar = getSupportActionBar(); //앱바 제어를 위해 툴바 액세스
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        getSupportActionBar().setTitle("설정하셈"); // 툴바 제목 설정
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true); // 앱바에 뒤로가기 버튼 만들기
+        }
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.bottom_home:
+                        Intent it = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(it);
+                        finish();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    /** Toolbar Item 선택시 기능 **/
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                //Intent it = new Intent(getApplicationContext(), StartActivity.class);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/cookandroid/project_mobile/StartActivity.java
@@ -0,0 +1,29 @@
+package com.cookandroid.project_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class StartActivity extends AppCompatActivity {
+    ImageView startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(it);
+                //finish();
+            }
+        });
+    }
+}
