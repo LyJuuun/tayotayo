9f306765d1408520109b817931ce947b86218a9c
cj2174 <cj2174@naver.com>
2023-05-23 23:35:28 +0900

    github push test1

--- app/src/main/java/com/cookandroid/project_mobile/StartActivity.java
+++ app/src/main/java/com/cookandroid/project_mobile/StartActivity.java
@@ -11,7 +11,7 @@ import android.widget.ImageView;
 public class StartActivity extends AppCompatActivity {
     ImageView startBtn;
     // hi
-    //test1234
+    //test123
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
